0b619f3bf83b0f2b08abc6392c7b16f988329226
Yong-il Cho <rushtenm@gmail.com>
2013-03-11 03:22:00 +0900

    Copy form Ex.

--- Chasing/Chasing/Chasing/Game1.cs
+++ Chasing/Chasing/Chasing/Game1.cs
@@ -16,15 +16,53 @@ namespace Chasing
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        KeyboardState lastKeyboardState = new KeyboardState();
+        KeyboardState currentKeyboardState = new KeyboardState();
+
+        CameraTarget target;
+        ChaseCamera camera;
+
+        Model targetModel;
+
+        bool cameraSpringEnabled = true;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 1366;
+            graphics.PreferredBackBufferHeight = 768;
+
+            // Create the chase camera
+            camera = new ChaseCamera();
+
+            // Set the camera offsets
+            camera.DesiredPositionOffset = new Vector3(0.0f, 2000.0f, 3500.0f);
+            camera.LookAtOffset = new Vector3(0.0f, 150.0f, 0.0f);
+
+            // Set camera perspective
+            camera.NearPlaneDistance = 10.0f;
+            camera.FarPlaneDistance = 100000.0f;
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            target = new CameraTarget(GraphicsDevice);
+
+            // Set the camera aspect ratio
+            // This must be done after the class to base.Initalize() which will
+            // initialize the graphics device.
+            camera.AspectRatio = graphics.GraphicsDevice.Viewport.AspectRatio;
+
+
+            // Perform an inital reset on the camera so that it starts at the resting
+            // position. If we don't do this, the camera will start at the origin and
+            // race across the world to get behind the chased object.
+            // This is performed here because the aspect ratio is needed by Reset.
+            UpdateCameraChaseTarget();
+            camera.Reset();
 
             base.Initialize();
         }
@@ -35,6 +73,7 @@ namespace Chasing
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            targetModel = Content.Load<Model>("Ship");
         }
 
         protected override void UnloadContent()
@@ -49,8 +88,44 @@ namespace Chasing
                 this.Exit();
 
             // TODO: Add your update logic here
+            lastKeyboardState = currentKeyboardState;
+
+            currentKeyboardState = Keyboard.GetState();
+
+            // Pressing the A button or key toggles the spring behavior on and off
+            if (lastKeyboardState.IsKeyUp(Keys.A) && (currentKeyboardState.IsKeyDown(Keys.A)))
+            {
+                cameraSpringEnabled = !cameraSpringEnabled;
+            }
+
+            // Reset the ship on R key or right thumb stick clicked
+            if (currentKeyboardState.IsKeyDown(Keys.R))
+            {
+                target.Reset();
+                camera.Reset();
+            }
+
+            // Update the ship
+            target.Update(gameTime);
+
+            // Update the camera to chase the new target
+            UpdateCameraChaseTarget();
+
+            // The chase camera's update behavior is the springs, but we can
+            // use the Reset method to have a locked, spring-less camera
+            if (cameraSpringEnabled)
+                camera.Update(gameTime);
+            else
+                camera.Reset();
 
             base.Update(gameTime);
+        }
+
+        private void UpdateCameraChaseTarget()
+        {
+            camera.ChasePosition = target.Position;
+            camera.ChaseDirection = target.Direction;
+            camera.Up = target.Up;
         }
 
         protected override void Draw(GameTime gameTime)
@@ -58,8 +133,31 @@ namespace Chasing
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+
+            DrawModel(targetModel, target.World);
 
             base.Draw(gameTime);
+        }
+
+        private void DrawModel(Model model, Matrix world)
+        {
+            Matrix[] transforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(transforms);
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.World = transforms[mesh.ParentBone.Index] * world;
+
+                    // Use the matrices provided by the chase camera
+                    effect.View = camera.View;
+                    effect.Projection = camera.Projection;
+                }
+                mesh.Draw();
+            }
         }
     }
 }
